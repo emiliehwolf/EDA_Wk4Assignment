c4b247da696b698049fafa933db487f918aa94c2
Emilie Wolf <emilie.h.wolf@gmail.com>
2017-05-26 01:42:18 -0500

    upload pm25analysis.R first draft (only headers)

---
+++ pm25analysis.R
@@ -0,0 +1,15 @@
+## pm25analysis.R
+## Author: Emilie H. Wolf
+## Date:
+## Description: This is the final peer-review assignment, (AKA Course Project 2)
+## for Coursera's Exploratory Data Analysis class.
+## URL of Data: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip
+
+## Please ensure this R script is in the same folder as the .rds files and
+## that your working directory is set to that folder.
+
+## Before starting my analysis, 
+## I downloaded and unzipped the datasets and moved them to a new folder called
+## EDA_Wk4Assignment. Next I opened SourceTree and created a new repository.
+## Then I opened RStudio and started a new R Project for that folder.
+## Now as I work on writing the script, I can save my progess to my GitHub.
